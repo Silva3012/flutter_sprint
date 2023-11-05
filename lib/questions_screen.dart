20ed52171d8ea87758f88760dca0094c47162041
Silva3012 <silvano.n92@gmail.com>
2023-11-05 09:24:39 +0200

    Style the answer buttons

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sprint/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -16,22 +17,16 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text("The question goes here..."),
+          const Text(
+            "The question goes here...",
+            style: TextStyle(color: Colors.white),
+          ),
           const SizedBox(
             height: 30,
           ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Answer 1"),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Answer 2"),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text("Answer 3"),
-          ),
+          AnswerButton("Answer 1", () {}),
+          AnswerButton("Answer 2", () {}),
+          AnswerButton("Answer 3", () {}),
         ],
       ),
     );
