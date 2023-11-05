b7a8d6ff8723e4078fd723bd1e81210fa721d568
Silva3012 <silvano.n92@gmail.com>
2023-11-05 09:49:01 +0200

    Access data from questions and display it

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sprint/answer_button.dart';
+import 'package:flutter_sprint/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,20 +14,23 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            "The question goes here...",
-            style: TextStyle(color: Colors.white),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
           ),
           const SizedBox(
             height: 30,
           ),
-          AnswerButton("Answer 1", () {}),
-          AnswerButton("Answer 2", () {}),
-          AnswerButton("Answer 3", () {}),
+          AnswerButton(currentQuestion.answers[0], () {}),
+          AnswerButton(currentQuestion.answers[1], () {}),
+          AnswerButton(currentQuestion.answers[2], () {}),
+          AnswerButton(currentQuestion.answers[3], () {}),
         ],
       ),
     );
