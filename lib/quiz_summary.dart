bb6025956ed554f182fcdaee34ae24541e932c2d
Silva3012 <silvano.n92@gmail.com>
2023-11-06 15:22:06 +0200

    Make the content scrollable

--- lib/quiz_summary.dart
+++ lib/quiz_summary.dart
@@ -7,28 +7,33 @@ class QuizSummary extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Column(
-      children: dataSummary.map(
-        (data) {
-          return Row(
-            children: [
-              Text(((data["question_index"] as int) + 1).toString()),
-              Expanded(
-                child: Column(
-                  children: [
-                    Text(data["question"] as String),
-                    const SizedBox(
-                      height: 5,
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: dataSummary.map(
+            (data) {
+              return Row(
+                children: [
+                  Text(((data["question_index"] as int) + 1).toString()),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(data["question"] as String),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(data["user_answer"] as String),
+                        Text(data["correct_answer"] as String),
+                      ],
                     ),
-                    Text(data["user_answer"] as String),
-                    Text(data["correct_answer"] as String),
-                  ],
-                ),
-              )
-            ],
-          );
-        },
-      ).toList(),
+                  )
+                ],
+              );
+            },
+          ).toList(),
+        ),
+      ),
     );
   }
 }
